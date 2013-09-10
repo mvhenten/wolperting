e78670773fae518ff64381cf11df400030d6f573
Matthijs van Henten <matthijs@ischen.nl>
2013-09-10 08:58:25 +0200

    add working test

--- test/poo.js
+++ test/poo.js
@@ -1,5 +1,6 @@
 var assert = require('assert'),
     _ = require('lodash'),
+    Faker = require('Faker'),
     Poo = require('../');
 
 
@@ -23,16 +24,37 @@ suite('poo constructors and tests', function(){
         }
     });
 
-    test('Instantiate Poo objects', function(){
-        var x = _.random(), y = _.random();
+    test('Instantiate Poo objects', function(done){
+        _.range( 0, 999 ).forEach(function(){
+            var x = _.random(0, 999),
+                y = _.random(0, 999);
 
-        var p = new Point({
-            x: x,
-            y: x
-        });
+            var p = new Point({
+                x: x,
+                y: y
+            });
 
-        assert.equal( p.x, x );
-        assert.equal( p.y, y );
+            assert.equal( p.x, x, 'Constructor has set the expected value');
+            assert.equal( p.y, y, 'Constructor has set the expected value');
 
+            assert.throws(function(){
+                p.x = Faker.Lorem.words();
+            }, /Error: Attempting to write property x/);
+
+            assert.throws(function(){
+                p.y = Faker.Lorem.words();
+            }, /Error: Attempting to write property y/);
+
+
+            assert.throws(function(){
+                var p = new Point({
+                    x: Faker.Lorem.words(),
+                    y: y
+                });
+            }, /TypeError: TypeConstraint Failed: value for x is not a/ );
+
+     });
+
+        done();
     })
 });
